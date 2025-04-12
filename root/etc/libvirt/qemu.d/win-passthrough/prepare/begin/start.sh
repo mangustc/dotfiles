720bc158cd686c8b642e49b652abbb03b23fe05d
Ivan Lifanov <letalbark@gmail.com>
2025-04-12 16:34:04 +0700

    remove unnecessary commands in win-passthrough start and stop scripts

--- root/etc/libvirt/qemu.d/win-passthrough/prepare/begin/start.sh
+++ root/etc/libvirt/qemu.d/win-passthrough/prepare/begin/start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -x
 
-echo 0 > /sys/class/vtconsole/vtcon0/bind
-echo 0 > /sys/class/vtconsole/vtcon1/bind
 echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
-
-modprobe -r nvidia_drm nvidia_modeset nvidia_uvm nvidia
-modprobe vfio-pci
+modprobe -r nvidia_drm nvidia_modeset nvidia_uvm nvidia snd_hda_intel
 
 systemctl set-property --runtime -- system.slice AllowedCPUs=5
 systemctl set-property --runtime -- user.slice AllowedCPUs=5
 systemctl set-property --runtime -- init.scope AllowedCPUs=5
-
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
--- root/etc/libvirt/qemu.d/win-passthrough/release/end/stop.sh
+++ root/etc/libvirt/qemu.d/win-passthrough/release/end/stop.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 set -x
 
-modprobe -r vfio-pci
+systemctl set-property --runtime -- system.slice AllowedCPUs=0-5
+systemctl set-property --runtime -- user.slice AllowedCPUs=0-5
+systemctl set-property --runtime -- init.scope AllowedCPUs=0-5
+echo powersave | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+echo balance_performance | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 
 echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
-
 modprobe nvidia_drm
 modprobe nvidia_modeset
 modprobe nvidia_uvm
 modprobe nvidia
-
-# Bind VTconsoles: might not be needed
-echo 1 > /sys/class/vtconsole/vtcon0/bind
-echo 1 > /sys/class/vtconsole/vtcon1/bind
-
-# Restart Display Manager
-# systemctl start display-manager
-
-systemctl set-property --runtime -- system.slice AllowedCPUs=0-5
-systemctl set-property --runtime -- user.slice AllowedCPUs=0-5
-systemctl set-property --runtime -- init.scope AllowedCPUs=0-5
-
-echo powersave | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo balance_performance | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
+modprobe snd_hda_intel
