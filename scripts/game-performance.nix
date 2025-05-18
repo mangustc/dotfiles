12f313193fdfde3b2ab76375b1cd527e3dc71ec8
Ivan Lifanov <letalbark@gmail.com>
2025-05-18 21:43:51 +0700

    fix proton stuttering after about 30mins of gameplay, remove gamemoderun since it bring negligable perfromance diffrence

--- scripts/game-performance.nix
+++ scripts/game-performance.nix
@@ -2,12 +2,13 @@
 
 let
 	script = pkgs.writeScriptBin "game-performance" ''
+export LD_PRELOAD=""
 export DXVK_FILTER_DEVICE_NAME="NVIDIA"
 export VKD3D_FILTER_DEVICE_NAME="NVIDIA"
 
 systemctl start scx.service
 cpuperf performance
-gamemoderun "$@"
+"$@"
 cpuperf powersave
 systemctl stop scx.service
 '';
