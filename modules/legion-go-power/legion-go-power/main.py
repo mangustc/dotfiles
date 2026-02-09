fd6b87f0db62caab578cedf22bd03218b538c0cf
Ivan Lifanov <letalbark@gmail.com>
2026-02-09 21:20:07 +0700

    allow user to change legion-go-power config without root, also make the service accessible to the user

--- modules/legion-go-power/legion-go-power/main.py
+++ modules/legion-go-power/legion-go-power/main.py
@@ -3,15 +3,42 @@ import cpu
 import logging
 import subprocess
 import tomllib
+import sys
+import os
+import shutil
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+logger = logging.getLogger(__name__)
 
 # TODO: add some way to change and view these values in plasma and steam gamescope session
 
-with open('/etc/legion-go-power.toml', 'rb') as f:
+if len(sys.argv) > 1:
+    user = sys.argv[1]
+else:
+    logger.info("You should provide user to start the service")
+    sys.exit(1)
+
+# Config paths
+user_config_dir = f'/home/{user}/.config/legion-go-power'
+user_config_path = f'{user_config_dir}/config.toml'
+system_config_path = '/usr/local/lib/legion-go-power/config.toml'
+user_uid = int(subprocess.check_output(f'id -u {user}', shell=True).decode().strip())
+user_gid = int(subprocess.check_output(f'id -g {user}', shell=True).decode().strip())
+
+os.makedirs(user_config_dir, mode=0o755, exist_ok=True)
+if not os.path.exists(user_config_path):
+    logger.info(f"User config not found at {user_config_path}, copying from system template")
+    shutil.copy2(system_config_path, user_config_path)
+
+os.chown(user_config_path, user_uid, user_gid)
+os.chmod(user_config_path, 0o644)
+os.chown(user_config_dir, user_uid, user_gid)
+os.chmod(user_config_dir, 0o755)
+
+with open(user_config_path, 'rb') as f:
     config = tomllib.load(f)
 
 # Lenovo settings
