8f2497d0f2443d16182e26c7694bcebbe70535a2
Ivan Lifanov <letalbark@gmail.com>
2026-03-13 22:29:01 +0700

    add windows page size scripts

---
+++ windows/install-scripts/set-page-size-10gb-ADMIN.bat
@@ -0,0 +1,3 @@
+@findstr/v "^@f.*&" "%~f0"|powershell -&goto:eof
+Get-CimInstance Win32_ComputerSystem | Set-CimInstance -Property @{AutomaticManagedPagefile = $false}
+Get-CimInstance -ClassName Win32_PageFileSetting | Where-Object { $_.Name -eq "$env:SystemDrive\pagefile.sys" } | Set-CimInstance -Property @{ InitialSize = 10240; MaximumSize = 10240}
