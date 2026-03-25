2add4cd9dc2f593bb6498a37a82ec99b4309a009
Ivan Lifanov <letalbark@gmail.com>
2026-03-25 19:28:54 +0700

    remove not working kernel parameters from sysctl

--- configuration-desktop.sh
+++ configuration-desktop.sh
@@ -15,18 +15,11 @@ config swap --size 16G
 config v226hql --output-name HDMI-A-1
 
 config sysctl --opts "
-net.ipv4.tcp_mtu_probing = true
 net.ipv4.tcp_fin_timeout = 5
 kernel.split_lock_mitigate = 0
 kernel.nmi_watchdog = 0
 kernel.soft_watchdog = 0
 kernel.watchdog = 0
-kernel.sched_cfs_bandwidth_slice_u = 3000
-kernel.sched_latency_ns = 3000000
-kernel.sched_min_granularity_ns = 300000
-kernel.sched_wakeup_granularity_ns = 500000
-kernel.sched_migration_cost_ns = 50000
-kernel.sched_nr_migrate = 128
 vm.max_map_count = 2147483642
 "
 config module-blacklist --modules "
