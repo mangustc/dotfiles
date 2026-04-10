b169932615d2fb99d129d83d1be12659f62fb8d7
Ivan Lifanov <letalbark@gmail.com>
2026-04-11 00:01:53 +0700

    update chres resolutions

--- windows/chres/chres.bat
+++ windows/chres/chres.bat
@@ -2,33 +2,23 @@
 cls
 
 echo Select an option:
-choice /c:12345Q /m "1. 1920x1080@70, 2. 1600x900@75, 3. 1280x720@75, 4. 960x540@75, 5. 1920x1080@60, Q. Quit"
+choice /c:123Q /m "1. 1920x1080@60, 2. 1920x1080@70, 3. 1920x1080@76, Q. Quit"
 
 if %ERRORLEVEL%==1 goto Option1
 if %ERRORLEVEL%==2 goto Option2
 if %ERRORLEVEL%==3 goto Option3
-if %ERRORLEVEL%==4 goto Option4
-if %ERRORLEVEL%==5 goto Option5
-if %ERRORLEVEL%==5 goto End
+if %ERRORLEVEL%==4 goto End
 
 :Option1
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1920 -Height 1080 -Frequency 70
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1920 -Height 1080 -Frequency 60
 goto End
 
 :Option2
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1600 -Height 900 -Frequency 75
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1920 -Height 1080 -Frequency 70
 goto End
 
 :Option3
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1280 -Height 720 -Frequency 75
-goto End
-
-:Option4
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 960 -Height 540 -Frequency 75
-goto End
-
-:Option5
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1920 -Height 1080 -Frequency 60
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1920 -Height 1080 -Frequency 76
 goto End
 
 :End
