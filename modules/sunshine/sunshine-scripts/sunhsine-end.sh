e07fa1d36f6ee6390eb3dcc4cf3d828302b82420
Ivan Lifanov <letalbark@gmail.com>
2026-04-21 02:09:52 +0700

    add sunshine scripts

---
+++ modules/sunshine/sunshine-scripts/sunhsine-end.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+kscreen-doctor output.HDMI-A-1.mode.1920x1080@76 && kscreen-doctor output.HDMI-A-1.scale.1
---
+++ modules/sunshine/sunshine-scripts/sunhsine-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+kscreen-doctor output.HDMI-A-1.mode.2240x1400@60 && kscreen-doctor output.HDMI-A-1.scale.1.25
