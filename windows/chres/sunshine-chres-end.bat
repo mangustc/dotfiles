11ed0201fcff0c670f431940871a2456fa12ec61
Ivan Lifanov <letalbark@gmail.com>
2026-04-20 19:52:02 +0700

    update windows scripts and v226hql

---
+++ windows/chres/sunshine-chres-end.bat
@@ -0,0 +1,3 @@
+@echo off
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1920 -Height 1080 -Frequency 76
---
+++ windows/install-scripts/sunshine.bat
@@ -0,0 +1,3 @@
+@findstr/v "^@f.*&" "%~f0"|powershell -&goto:eof
+
+Start-Process "https://github.com/LizardByte/Sunshine/releases"
