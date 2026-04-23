dd71e3032eec0f70f9316ac3d77fb1b3b73fa6d3
Ivan Lifanov <letalbark@gmail.com>
2026-04-24 01:28:01 +0700

    update windows sunshine configuration

--- windows/chres/sunshine-chres-end.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 1920 -Height 1080 -Frequency 76
--- windows/chres/sunshine-chres-start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-ScreenResolution.ps1" -Width 2240 -Height 1400 -Frequency 60
