6497eaea29848fb3ad568214e72ac116e3f42f16
Ivan Lifanov <letalbark@gmail.com>
2026-04-30 23:36:55 +0700

    set display frequency fo v226hql

--- windows/sunshine/sunshine-end.bat
+++ windows/sunshine/sunshine-end.bat
@@ -1,5 +1,5 @@
 MultiMonitorTool.exe /disable MTT1337
 MultiMonitorTool.exe /enable ACR0335
-MultiMonitorTool.exe /SetPrimary ACR0335
+MultiMonitorTool.exe /SetMonitors "Name=ACR0335 Primary=1 DisplayFrequency=76 PositionX=0 PositionY=0"
 
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-MouseSpeed.ps1" -speed 4
