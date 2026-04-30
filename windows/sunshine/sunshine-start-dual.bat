5b3fea3622b291d1a46c652c6d611a7287efacc5
Ivan Lifanov <letalbark@gmail.com>
2026-04-30 23:32:25 +0700

    add set mouse to sunshine ual

--- windows/sunshine/sunshine-start-dual.bat
+++ windows/sunshine/sunshine-start-dual.bat
@@ -1,3 +1,5 @@
 MultiMonitorTool.exe /enable MTT1337
 MultiMonitorTool.exe /enable ACR0335
 MultiMonitorTool.exe /SetMonitors "Name=MTT1337 Primary=0 Width=%SUNSHINE_CLIENT_WIDTH% Height=%SUNSHINE_CLIENT_HEIGHT% DisplayFrequency=%SUNSHINE_CLIENT_FPS% PositionX=-%SUNSHINE_CLIENT_WIDTH% PositionY=0" "Name=ACR0335 Primary=1 PositionX=0 PositionY=0"
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0\Set-MouseSpeed.ps1" -speed 4
