39b2780c6511f1c8843a1bb3ae7a8c76a1400b6b
Ivan Lifanov <letalbark@gmail.com>
2026-06-11 10:50:05 +0700

    update laptop configuration

--- configuration-laptop.sh
+++ configuration-laptop.sh
@@ -5,58 +5,41 @@ export DOTFILES_HOST="laptop"
 export DOTFILES_DIR="$(realpath "$(dirname "$0")")"
 cd "$DOTFILES_DIR"
 source ./base.sh
+
 config base
-config yay
+config network
+config audio
+config ivo8c45 --output-name eDP-1
+config plasma
+config fonts
+config fish --zellij
+config swap --size 8G
+config zswap
+config amd
 
 # config
 install_pkgs "$(trim_pkgs_file "./packages-$DOTFILES_HOST")"
 
-# systemd-boot
-config swap --size 8G
-
-config acpi_call
-config module-blacklist --modules "
-pcscpkr
-iTCO_wdt
-sp5100_tco
-"
-config earlyoom
 config brightness
-config pipewire
-config networkmanager --proxy --doh-https "https://dns.quad9.net/dns-query" --doh-ipv4 "9.9.9.9" --doh-ipv4-alt "149.112.112.112"
-config nethandlerm
 config ssh-agent
 config dualsense
-config ppd
-config plasma
 config neovim
 config fish --zellij
 config archscripts
 config git
 config bluetooth
-config flatpak --flatpaks "
-"
-config sddm
 config scripts
 config konsole
-config zellij
-config fonts
 config launch-windows
-config zen-browser --search-engine
-config zswap
+config zen-browser
 config docker
 config discord
-config ivo8c45 --output-name eDP-1
-
-cmd install -D -m 644 ./modules/steam-session/steam.desktop ~/.local/share/applications/steam.desktop
 
 # late
 config plasma-LATE
 config boot-LATE --kernel-name linux
 config hosts-LATE
 config nftables-LATE
-config sysctl-LATE
-config env-LATE
 
 # end
 print_orphan_packages
